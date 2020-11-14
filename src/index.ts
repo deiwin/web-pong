7f5066ff76c610ae0c7379863e2adae48d0e06ff
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-11-14 13:54:19 +0200

    Rename ViewportState -> ViewportSize

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ interface Velocity {
   y: number;
 }
 
-interface ViewportState {
+interface ViewportSize {
   height: number;
   width: number;
 }
@@ -21,21 +21,21 @@ function createBallElement() {
   return element;
 }
 
-const getViewportState = (): ViewportState => ({
+const getViewportSize = (): ViewportSize => ({
   height: Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0),
   width: Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 });
 
-const updateVelocity = (velocity: Velocity, viewportState: ViewportState): Velocity => {
+const updateVelocity = (velocity: Velocity, viewportSize: ViewportSize): Velocity => {
   const rect = ball.getBoundingClientRect();
 
   const shouldFlipHorizontally =
-    (rect.right + velocity.x > viewportState.width && velocity.x > 0) ||
+    (rect.right + velocity.x > viewportSize.width && velocity.x > 0) ||
     (rect.left + velocity.x < 0 && velocity.x < 0)
   const xMultiplier = shouldFlipHorizontally ? -1 : 1;
 
   const shouldFlipVertically =
-    (rect.bottom + velocity.y > viewportState.height && velocity.y > 0) ||
+    (rect.bottom + velocity.y > viewportSize.height && velocity.y > 0) ||
     (rect.top + velocity.y < 0 && velocity.y < 0)
   const yMultiplier = shouldFlipVertically ? -1 : 1;
 
@@ -56,13 +56,13 @@ document.body.appendChild(ball);
 const initialVelocity: Velocity = {x: 4, y: 8};
 
 const ticks = interval(10).pipe(take(1000));
-const viewportStateObservable = fromEvent(window, 'resize').pipe(
+const viewportSizeObservable = fromEvent(window, 'resize').pipe(
   startWith({}),
-  map(_ => getViewportState())
+  map(_ => getViewportSize())
 );
 
 ticks.pipe(
-  withLatestFrom(viewportStateObservable),
-  map(([_, viewportState]) => viewportState),
+  withLatestFrom(viewportSizeObservable),
+  map(([_, viewportSize]) => viewportSize),
   scan(updateVelocity, initialVelocity)
 ).subscribe(updateWorld);
