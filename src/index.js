569b94121b7cff194372ea4ebbb2acd441a1a2e0
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-11-14 13:21:28 +0200

    Extract velocity calculations to a scanner

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { interval, fromEvent } from 'rxjs';
-import { take, map, startWith, withLatestFrom } from 'rxjs/operators';
+import { take, map, startWith, withLatestFrom, scan } from 'rxjs/operators';
 
 function createBallElement() {
   const element = document.createElement('div');
@@ -16,34 +16,43 @@ const getViewportWidth = () =>
 const getViewportHeight = () =>
   Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
+const updateVelocity = (velocity, viewportState) => {
+  const rect = ball.getBoundingClientRect();
+
+  const shouldFlipHorizontally =
+    (rect.right + velocity.x > viewportState.width && velocity.x > 0) ||
+    (rect.left + velocity.x < 0 && velocity.x < 0)
+  const xMultiplier = shouldFlipHorizontally ? -1 : 1;
+
+  const shouldFlipVertically =
+    (rect.bottom + velocity.y > viewportState.height && velocity.y > 0) ||
+    (rect.top + velocity.y < 0 && velocity.y < 0)
+  const yMultiplier = shouldFlipVertically ? -1 : 1;
+
+  return {
+    x: xMultiplier * velocity.x,
+    y: yMultiplier * velocity.y,
+  }
+};
+
+const updateWorld = (velocity) => {
+  ball.style.left = `${ball.offsetLeft + velocity.x}px`;
+  ball.style.top = `${ball.offsetTop + velocity.y}px`;
+};
+
 const ball = createBallElement();
 document.body.appendChild(ball);
 
+const initialVelocity = {x: 4, y: 8};
+
+const ticks = interval(10).pipe(take(1000));
 const viewportStateObservable = fromEvent(window, 'resize').pipe(
   startWith({}),
   map(_ => ({height: getViewportHeight(), width: getViewportWidth()}))
 );
 
-
-var velocity = {x: 4, y: 8};
-const ticks = interval(10).pipe(
-  take(1000),
-  withLatestFrom(viewportStateObservable)
-);
-ticks.subscribe(([_, viewportState]) => {
-  const rect = ball.getBoundingClientRect();
-  const shouldFlipHorizontally =
-    (rect.right + velocity.x > viewportState.width && velocity.x > 0) ||
-    (rect.left + velocity.x < 0 && velocity.x < 0)
-  if (shouldFlipHorizontally) {
-    velocity.x *= -1;
-  }
-  const shouldFlipVertically =
-    (rect.bottom + velocity.y > viewportState.height && velocity.y > 0) ||
-    (rect.top + velocity.y < 0 && velocity.y < 0)
-  if (shouldFlipVertically) {
-    velocity.y *= -1;
-  }
-  ball.style.left = `${ball.offsetLeft + velocity.x}px`;
-  ball.style.top = `${ball.offsetTop + velocity.y}px`;
-});
+ticks.pipe(
+  withLatestFrom(viewportStateObservable),
+  map(([_, viewportState]) => viewportState),
+  scan(updateVelocity, initialVelocity)
+).subscribe(updateWorld);
