6402f151440c0af5c9d12e49f61f27b8d7c582e3
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-11-14 23:11:16 +0200

    Store ball location in state
    Previously the location was stored in the DOM and fetched with
    `getBoundingClientRect`. Now this DOM function call is not required.

--- src/ball.ts
+++ src/ball.ts
@@ -1,14 +1,22 @@
 import { ViewportSize } from './viewport';
 
 export interface BallState {
+  topLeft: Point;
   velocity: Velocity;
+}
+interface Point {
+  x: number;
+  y: number;
 }
 interface Velocity {
   x: number;
   y: number;
 }
 
-export const initialBallState = { velocity: { x: 4, y: 8 } };
+export const initialBallState = {
+  topLeft: { x: 0, y: 0 },
+  velocity: { x: 4, y: 8 },
+};
 
 function createBallElement(): HTMLElement {
   const element = document.createElement('div');
@@ -23,21 +31,29 @@ const ball = createBallElement();
 document.body.appendChild(ball);
 
 export const updateBallState = (viewportSize: ViewportSize) => ({
+  topLeft,
   velocity,
 }: BallState): BallState => {
-  const rect = ball.getBoundingClientRect();
+  const left = topLeft.x;
+  const right = left + ball.offsetWidth;
+  const top = topLeft.y;
+  const bottom = top + ball.offsetHeight;
 
   const shouldFlipHorizontally =
-    (rect.right + velocity.x > viewportSize.width && velocity.x > 0) ||
-    (rect.left + velocity.x < 0 && velocity.x < 0);
+    (right + velocity.x > viewportSize.width && velocity.x > 0) ||
+    (left + velocity.x < 0 && velocity.x < 0);
   const xMultiplier = shouldFlipHorizontally ? -1 : 1;
 
   const shouldFlipVertically =
-    (rect.bottom + velocity.y > viewportSize.height && velocity.y > 0) ||
-    (rect.top + velocity.y < 0 && velocity.y < 0);
+    (bottom + velocity.y > viewportSize.height && velocity.y > 0) ||
+    (top + velocity.y < 0 && velocity.y < 0);
   const yMultiplier = shouldFlipVertically ? -1 : 1;
 
   return {
+    topLeft: {
+      x: topLeft.x + xMultiplier * velocity.x,
+      y: topLeft.y + yMultiplier * velocity.y,
+    },
     velocity: {
       x: xMultiplier * velocity.x,
       y: yMultiplier * velocity.y,
@@ -45,7 +61,7 @@ export const updateBallState = (viewportSize: ViewportSize) => ({
   };
 };
 
-export const updateWorldBall = (ballState: BallState) => {
-  ball.style.left = `${ball.offsetLeft + ballState.velocity.x}px`;
-  ball.style.top = `${ball.offsetTop + ballState.velocity.y}px`;
+export const updateWorldBall = ({ topLeft: { x, y } }: BallState) => {
+  ball.style.left = `${x}px`;
+  ball.style.top = `${y}px`;
 };
